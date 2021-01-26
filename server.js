61e5f7771ea3b509ebf3dc868b82666ccaa6d269
DoniZhelev <donizhelev@gmail.com>
2021-01-26 20:48:43 +0200

    add express handlebars and html doc

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 const express = require('express');
+const handlebars  = require('express-handlebars');
+
+const checkCatIdMiddleware = require('./middlewares/middleware');
+const logger = require('./middlewares/loggerMiddleware');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log("Hello from console");
+app.use(logger)
+app.engine('hbs', handlebars());
+app.set('view engine', 'hbs')
 
-    res.send('Hello world from exrepss!')
+app.get('/', (req, res) => {
+    
+    let name = 'gosHo';
+
+    res.render('home', { layout: false, name: name })
 });
 
-app.get('/cats/:catId', (req, res) =>{
+app.get('/download', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/cats', (req, res) =>{
+    res.redirect('/')
+})
+
+app.get('/cats/:catId',checkCatIdMiddleware, (req, res) =>{
     console.log(req.params);
     res.send(`You are looking at profile of ${req.params.catId}`)
 })
 
 app.post('/cats', (req, res) => {
     console.log('create cat');
-    res.send('cat Created!')
+   res.status(201).res.send('cat Created!')
 });
 
 app.listen(5000, () => console.log(`Server is running on port 5000`))
